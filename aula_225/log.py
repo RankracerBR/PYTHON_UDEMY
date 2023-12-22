a7d3a845352c8dacc31754cd25338c7966cb7e49
Augusto Pontes <augustopontes010@gmail.com>
2023-12-22 10:00:41 -0300

    aulas

--- aula_225/log.py
+++ aula_225/log.py
@@ -1,12 +1,23 @@
 #Abstração
 class Log:
-    def log(self, msg): #Assinatura do método
+    def _log(self, msg): #Assinatura do método
         raise NotImplementedError('Implemente o método log')
 
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+
 class LogFileMixin(Log):
-    def log(self, msg):
+    def _log(self, msg):
         print(msg)
 
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
 if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log('qualquer coisa')
+    l = LogPrintMixin()
+    l.log_error('qualquer coisa')
+    l.log_success('Que Legal :3')
