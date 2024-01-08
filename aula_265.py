da6fa74ad04eea6f286f1695aad33293d5f0e25e
Augusto Pontes <augustopontes010@gmail.com>
2024-01-08 05:28:24 -0300

    aulas

--- aula_265.py
+++ aula_265.py
@@ -1,31 +1,16 @@
-from dataclasses import dataclass, asdict, astuple
+from dataclasses import dataclass, field, fields
 
 @dataclass(repr=True)
 class Pessoa:
-    nome: str
-    sobrenome: str
+    nome: str = field(
+        default='MISSING',
+    )
+    sobrenome: str = 'Not sent'
+    idade: int = 100
+    enderecos: list[str] = field(default_factory=list)
 
-    #def __init__(self, nome, sobrenome):
-        #self.nome = nome
-        #self.sobrenome = sobrenome
-        #self.nome_completo = f'{self.nome} {self.sobrenome}'
-
-    #def __post_init__(self):
-        #print('POST INIT')
-
-    #@property
-    #def nome_completo(self):
-        #return f'{self.nome} {self.sobrenome}'
-
-    #@nome_completo.setter
-    #def nome_completo(self, valor):
-        #nome, *sobrenome = valor.split()
-        #self.nome = nome
-        #self.sobrenome = ''.join(sobrenome)
 
 if __name__ == '__main__':
-    p1 = Pessoa('Augusto', 'Pontes')
-    print(asdict(p1).keys())
-    print(asdict(p1).values())
-    print(asdict(p1).items())
-    print(astuple(p1)[0])
+    p1 = Pessoa()
+    print(fields(p1))
+    print(p1)
