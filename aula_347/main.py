0310f72193d6d551212cede7c8445f899d382e0b
RankracerBR <augustopontes010@gmail.com>
2024-02-12 12:40:21 -0300

    aulas

--- aula_347/main.py
+++ aula_347/main.py
@@ -2,6 +2,9 @@ from PySide6.QtWidgets import (QApplication,QLabel)
 from variables import WINDOW_ICON_PATH
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
+from display import Display
+from info import Info
+from styles import setuptheme
 import sys
 
 def temp_label(texto):
@@ -16,12 +19,23 @@ if __name__ == '__main__':
 
     # Cria a aplicação
     app = QApplication(sys.argv)
+    setuptheme()
     window = MainWindow()
 
-    # Define o icon
+    # Define o ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
 
+    # Info
+    info = Info('2.0 ^ 10.0')
+    window.addWidgetToVLayout(info)
+
+    # Display
+    display = Display()
+    display.setPlaceholderText('Digite algo')
+    window.addWidgetToVLayout(display)
+
+    # Exec
     window.show()
     app.exec()
---
+++ aula_347/styles.py
@@ -0,0 +1,32 @@
+from variables import PRIMARY_COLOR, DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR
+import qdarktheme
+
+qss = f"""
+    QPushButton[cssClass="specialButton"] {{
+        color: #fff;
+        background: {PRIMARY_COLOR};
+    }}
+    QPushButton[cssClass="specialButton"]:hover {{
+        color: #fff;
+        background: {DARKER_PRIMARY_COLOR};
+    }}
+    QPushButton[cssClass="specialButton"]:pressed {{
+        color: #fff;
+        background: {DARKEST_PRIMARY_COLOR};
+    }}
+"""
+
+def setuptheme():
+    qdarktheme.setup_theme(
+        theme='light',
+        corner_shape='rounded',
+        custom_colors={
+            "[dark]": {
+                "primary": f"{PRIMARY_COLOR}",
+            },
+            "[light]": {
+                "primary": f"{PRIMARY_COLOR}",
+            },
+        },
+        additional_qss=qss
+    )
