a6cd38335f01cdceac42b3005eee77a793b600be
RankracerBR <augustopontes010@gmail.com>
2024-03-01 00:35:57 -0300

    mysql

---
+++ aula_399/main.py
@@ -0,0 +1,12 @@
+import pymysql
+
+connection = pymysql.connect(
+    host = 'localhost',
+    user = 'usuario',
+    password = 'senha',
+    database = 'base_de_dados',
+)
+
+with connection:
+    with connection.cursor() as cursor:
+        print(cursor)
