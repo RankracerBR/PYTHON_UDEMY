74515fcbce7a892c7d06b76928fea98f8820dede
RankracerBR <augustopontes010@gmail.com>
2024-03-01 08:02:12 -0300

    mysql

--- aula_399/main.py
+++ aula_399/main.py
@@ -1,12 +1,47 @@
 import pymysql
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+TABLE_NAME = 'customers'
 
 connection = pymysql.connect(
-    host = 'localhost',
-    user = 'usuario',
-    password = 'senha',
-    database = 'base_de_dados',
+    host = os.environ['MYSQL_HOST'],
+    user = os.environ['MYSQL_USER'],
+    password = os.environ['MYSQL_PASSWORD'],
+    database = os.environ['MYSQL_DATABASE'],
 )
+
+print(os.environ['MYSQL_DATABASE'])
 
 with connection:
     with connection.cursor() as cursor:
-        print(cursor)
+        cursor.execute(
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+            'id INT NOT NULL AUTO_INCREMENT, '
+            'nome VARCHAR(50) NOT NULL, '
+            'idade INT NOT NULL, '
+            'PRIMARY KEY (id)'
+            ')'
+        )
+        # CUIDADO: ISSO LIMPA A TABELA
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+    connection.commit()
+
+    # Começo a manipular dados a partir daqui
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES ("Augusto", 20) '
+        )
+        cursor.execute(
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES ("Augusto", 20) '
+        )
+        result = cursor.execute(
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES ("Augusto", 20) '
+        )
+        print(result)
+    connection.commit()
